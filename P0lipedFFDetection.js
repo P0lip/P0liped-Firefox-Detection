9ad088c52d04dfe9b70b25b62bfb34038e1860b0
P0lip <P0lip@users.noreply.github.com>
2014-11-15 17:14:06 +0100

    Update P0lipedFFDetection.js
    Small tweaks

--- P0lipedFFDetection.js
+++ P0lipedFFDetection.js
@@ -1,46 +1,59 @@
-/* P0liped FF Detection | GNU GPLv3 */
-var p0FF = (function(document, window, undefined){
+/* P0liped FF Detection 1.0 | GNU GPLv3 
+Last update: 10.11.2014 
+I'd like to thank @Ginden (you can find him on GitHub) for his help with detecting native functions (isFunctioNative was forked from his amazing project). */
+var p0FF = (function(document, window, undefined){ // It returns a number with proper version of Firefox.
   "use strict";
-  if(document.documentElement.style.MozAppearance === undefined) // CSS Appearance with "moz" prefix is available in all version starting from 2 (probably in lower than 2 aswell) to 36 (the latest release as of 03.11.2014)
+  if(document.documentElement.style.MozAppearance === undefined) // CSS Appearance with "moz" prefix is available in all version starting from 2 (probably in lower than 2 aswell) to 36 (the latest release as of 10.11.2014)
 	return;
 
   var 
-	onlyLetters = /[^A-z]/, // regular expression matching all non-letter signs (it will be always true when something else than a letter occurs in the string)
-	createHTMLElement = function(tag){ // function creating a HTML Element
-	  if(typeof tag == "string" && !onlyLetters.test(tag))
-		return document.createElement(tag); // returns created element you may work with using JavaScript
+	onlyLetters = /[^A-z]/, // Regular expression matching all non-letter signs (always true when something else than a letter occurs in the string).
+	createHTMLElement = function(tag){ // Function creating a HTML Element
+	  return document.createElement(typeof tag == "string" && !onlyLetters.test(tag) ? tag : "p0lip"); // Returns the created element you may work with using JavaScript
 	},
-	isHTMLElementSupported = function(tag){  // function checking whether a HTML Element with the given tag is recognized by the browser or not.
-	  if(typeof tag == "string" && !onlyLetters.test(tag))
-		return Object.prototype.toString.call(createHTMLElement(tag)) !== "[object HTMLUnknownElement]"; // if the browser doesn't recognize the tag, the element created is equal to HTMLUnknownElement 
-	},
-	isFunctionNative = function(){ // function checking if the function passed as an argument is native or not
-	  var stripFunction = /.+ /; // regular expression which removes all signs until { occurs. I don't use Funcion.prototype.bind (it would be a better solution), because it's not supported in lower versions and a workaround would take 5-6 lines. If it's ok for you, let me know and I'll write a one with bind instead of this regular expression
-	  return function(func){
-		return func !== undefined && Function.prototype.toString.call(func).replace(stripFunction, "") === "{\n    [native code]\n}"; 
+	isHTMLElementSupported = function(){  // Function that checks whether a HTML Element with the given tag is recognized by the browser or not. Returns a boolean.
+	  var toString = Object.prototype.toString; // We cache this function as a local variable for futher use.
+	  return function(tag){
+		toString.call(createHTMLElement(tag)) !== "[object HTMLUnknownElement]"; // When the browser doesn't recognize the element, it's always treated as HTMLUnknownElement.
 	  };
 	}(),
-	isCSSFeatureSupported = window.CSS && isFunctionNative(window.CSS.supports) ? function(){ // checks whether a given CSS feature is supported or not
-	  return window.CSS.supports.apply(window.CSS, arguments); // using native method available in FF >= 22
-	} : function(){ // fallback method used in FF < 22
-	  var HTMLElement = document.createDocumentFragment().appendChild(createHTMLElement("div"));
+	isFunctionNative = function(){ // Function checking if the function passed as an argument is native or not. Returns a boolean.
+	  var toString = Function.prototype.toString; // We cache this function as a local variable for further use.
+	  return function(func){ // Forked from https://github.com/Ginden/reflect-helpers/blob/master/reflect-helpers.js. All rights belong to the author - Michał Wada aka @Ginden. I advise you to watch his outstanding stuff.
+		if(func === undefined)
+		  return false;
+		try { 
+		  Function("return (" + toString.call(func) + ")");
+		  return false;
+		} catch (e) {
+		  return true;
+		}
+	  };
+	}(),
+	isCSSFeatureSupported = window.CSS && isFunctionNative(window.CSS.supports) ? function(){ // Checks whether a given CSS feature is supported or not. Returns a boolean.
+	  return window.CSS.supports.apply(window.CSS, arguments); // Using native method which is available in FF >= 22
+	} : function(){ // Fallback method used by me for FF < 22. If you consider using it as in your feature detection code, notice it may not work properly in Internet Explorer. My tip: you'd better try HTMLElement.style[type] = value instead of cssText.
+	  var HTMLElement = document.createDocumentFragment().appendChild(createHTMLElement("div")); // We create a document fragment and then append a DIV element. It's optional, but I do it for sanitization reasons.
 	  return function(){
 		HTMLElement.style.cssText = [].join.call(arguments, ":");
 		return HTMLElement.style.cssText !== undefined && HTMLElement.style.cssText !== "";
 	  };
 	}(),
-	allTests = {
-	  35: function(){ 
-		  try {
-			String(Symbol("P0lip")) === "(\"Symbol(P0lip)\")"; 
-			return true; 
-		  } catch(e){
-			return false;
-		  }
-		},
-	  34: function(){
-		  return isFunctionNative(Object.prototype.assign);
+	allTests = { // Each test returns a boolean. 
+	  // Notice that 34, 35, 36 are currently in Beta, Aurora (which was actually renamed to Dev), Nightly stage, so you should assume that the result may be false. Just uncomment these 3 tests when you don't need them.
+	  /*36: function(){ // In 36 version the method of DOMMatrix object returns the proper CSS value for transforms instead of "[object DOMMatrix]".
+		try {
+		  return new window.DOMMatrix().toString() === "matrix(1, 0, 0, 1, 0, 0)";
+		} catch (e){
+		  return false;
+		}
 	  },
+	  35: function(){ 
+		return isFunctionNative(Element.prototype.closest);
+	  },
+	  34: function(){
+		  return isFunctionNative(String.prototype.raw) || isFunctionNative(String.raw);
+	  },*/
 	  33: function(){
 		return isCSSFeatureSupported("color", "rebeccapurple");
 	  },
@@ -120,7 +133,6 @@ var p0FF = (function(document, window, undefined){
 		} catch(e){
 		  return false;
 		}
-		
 	  },
 	  13: function(){
 		try{
@@ -172,12 +184,12 @@ var p0FF = (function(document, window, undefined){
 	};
 	
 	var order = function(){
-	  function sortNumbers(a, b){
+	  function sortNumbers(a, b){ // Function that sorts number from highest to lowest.
 		return b - a;
 	  }
-	  if(isFunctionNative(Object.keys)) // it's available in FF >= 4, 
+	  if(isFunctionNative(Object.keys)) // It's available in FF >= 4, 
 		return Object.keys(allTests).sort(sortNumbers);
-	  else { // we could assume the version number isn't higher than 4, but that might be tricky, as someone might have overwritten Object.keys and the statement above could be false.
+	  else { // We could assume the version number isn't higher than 4 (and iterate only last 4 tests), but that might be tricky, as someone might have overwritten Object.keys and the statement above could be false.
 		var result = [], prop;
 		for(prop in allTests)
 		  result.push(prop);
@@ -185,12 +197,12 @@ var p0FF = (function(document, window, undefined){
 	  }
 	}();
 	var result;
-	order.forEach(function(number, n){
+	order.forEach(function(number, i){
 	  if(result)
 		return false;
-	  if(allTests[number]() && (n !== 0 ? !allTests[order[n-1]]() : true))
+	  if(allTests[number]() && (i !== 0 ? !allTests[order[i-1]]() : true))
 		result = number;
 	});
-	return result;
+	return +result;
 
 })(document, window);
